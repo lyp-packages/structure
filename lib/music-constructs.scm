580458d2e52133d18485b1bf3d39bd9aa1fc7120
Sharon Rosner <ciconia@gmail.com>
2017-01-15 00:36:30 +0100

    Add make-part command

--- lib/music-constructs.scm
+++ lib/music-constructs.scm
@@ -17,12 +17,31 @@
     
     (make-simultaneous-music musics)))
 
+(define (sly:make-partial-score-music path parts)
+  (let* ((leaf (sly:ref path))
+         (parts (lset-difference eq? parts '(global))) ; remove global part
+         (global (hash-ref-indiff leaf 'global))
+         (order (sly:order-parts sly:default-score-order parts))
+         (musics '())
+       )
+       
+    ;(if global (set! musics (list global)))
+    
+    (for-each
+      (lambda (group)
+        (let* ((group-music (sly:make-staff-group-music group leaf global)))
+          (set! musics (append-item musics group-music))))
+      order)
+    
+    (make-simultaneous-music musics)))
+
+
 (define (sly:make-staff-group-music group leaf global)
   (let* ((parts group)
          (musics (map
                    (lambda (p)
                      (let ((part-music (hash-ref-indiff leaf p)))
-                     (sly:make-part (symbol->string p) part-music global))) 
+                     (sly:make-part-music (symbol->string p) part-music global))) 
                    parts))
          (musics (make-simultaneous-music musics)))
     #{ \new StaffGroup \with { } { #musics } #} ))
@@ -38,7 +57,7 @@
         expr
         (make-sequential-music (append prefixes (list expr))))))
 
-(define (sly:make-part name expr global)
+(define (sly:make-part-music name expr global)
   (let* ((with-exprs #f)
          (expr (sly:prefix-part-music name expr))
          (expr (if global (make-simultaneous-music (list global expr)) expr))
--- lib/music.ly
+++ lib/music.ly
@@ -1,2 +1,7 @@
 make-score = #(define-scheme-function (path) (string-or-symbol?)
-  (scorify-music (sly:make-score-music path)))
+  (begin
+    (display (format "\npath: ~a\n" path))
+    (scorify-music (sly:make-score-music path))))
+  
+make-part = #(define-scheme-function (path part) (string-or-symbol? string-or-symbol?)
+  (scorify-music (sly:make-partial-score-music path (list part))))
