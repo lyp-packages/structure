f9a6393438aa7808333cd97b76b325c9b6d59b49
Sharon Rosner <ciconia@gmail.com>
2017-01-15 00:35:05 +0100

    Fix sly:alist-ref, refactor tree code

--- lib/tree.scm
+++ lib/tree.scm
@@ -13,20 +13,6 @@
                 (loop value path-parts)
                 #f))))))
 
-(define (sly:tree-ref-init-leaf tree path)
-  (let ((path (if (symbol? path) (symbol->string path) path)))
-    (let loop ((leaf tree) (path-parts (string-split path #\/)))
-      (let* ((key (car path-parts))
-             (value (hash-ref leaf key))
-             (path-parts (cdr path-parts))
-           )
-        (if (null? path-parts)
-            (if value value (hash-set! leaf key (make-hash-table)))
-            (if (hash-table? value)
-                (loop value path-parts)
-                #f))))))
-
-
 (define (sly:tree-set! tree path value)
   (let ((path (if (symbol? path) (symbol->string path) path)))
     (let loop ((leaf tree) (path-parts (string-split path #\/)))
@@ -42,5 +28,37 @@
                   (hash-set! leaf key subleaf)))
               (loop subleaf path-parts)))))))
 
+
+
+(define (sly:recursive-hash-table->alist hash-table)
+  (hash-fold
+    (lambda (k v r)
+      (let ((v (if (hash-table? v) (sly:recursive-hash-table->alist v) v)))
+        (append r (list (cons k v)))
+      )
+      ;(if (hash-table? v)
+        ;'(k (sly:recursive-hash-table->alist v))
+        ;'(k v)
+      
+    ) '() hash-table)
+)
+
+(define (sly:tree-alist-ref tree path)
+  (let ((path (if (symbol? path) (symbol->string path) path)))
+    (let loop ((leaf tree) (path-parts (string-split path #\/)))
+      (let* ((key (car path-parts))
+             (value (hash-ref leaf key))
+             (path-parts (cdr path-parts))
+           )
+        (if (null? path-parts) ; reached target
+            (if (hash-table? value)
+                (sly:recursive-hash-table->alist value)
+                value)
+            (if (hash-table? value)
+                (loop value path-parts)
+                #f))))))
+
 (define (sly:ref path) (sly:tree-ref sly:root path))
 (define (sly:set! path value) (sly:tree-set! sly:root path value))
+
+(define (sly:alist-ref path) (sly:tree-alist-ref sly:root path))
--- test/tree-test.ly
+++ test/tree-test.ly
@@ -1,22 +1,23 @@
 \version "2.19.37"
 \require "assert"
-\pinclude "../package.ly"
+\require "structure:.."
 
-#(begin
-  (sly:set! "life" 42)
-  (assert:eq? (sly:ref "life") 42)
+#(sly:set! "life" 42)
+#(assert:eq? (sly:ref "life") 42)
 
-  (sly:set! "x/y/z" 123)
-  (assert:eq? (sly:ref "x/y/z") 123)
+#(sly:set! "x/y/z" 123)
+#(assert:eq? (sly:ref "x/y/z") 123)
 
-  (sly:set! "abc" 1)
-  (sly:set! "def/ghi" 2)
-  (sly:set! 'def/jkl/mno 3)
+#(sly:set! "abc" 1)
+#(sly:set! "def/ghi" 2)
+#(sly:set! 'def/jkl/mno 3)
+
+#(assert:eq? (sly:ref "xxx") #f)
+#(assert:eq? (sly:ref "abc") 1)
+#(assert:eq? (sly:ref "abc/ddd") #f)
+#(assert:eq? (sly:ref 'def/ghi) 2)
+#(assert:eq? (sly:ref "def/xxx") #f)
+#(assert:eq? (sly:ref "def/jkl/mno") 3)
+
+#(assert:equal? (format "~a" (sly:alist-ref "def")) (format "~a" '((ghi . 2) (jkl . ((mno . 3))))))
   
-  (assert:eq? (sly:ref "xxx") #f)
-  (assert:eq? (sly:ref "abc") 1)
-  (assert:eq? (sly:ref "abc/ddd") #f)
-  (assert:eq? (sly:ref 'def/ghi) 2)
-  (assert:eq? (sly:ref "def/xxx") #f)
-  (assert:eq? (sly:ref "def/jkl/mno") 3)
-)
